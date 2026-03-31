a7ab2215d1c8fd11a40dea628847eacb0266c906
berkekirbas <berkekirbas@mail.ru>
2026-03-31 16:04:17 +0300

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,1 @@
-import axios from 'axios';
-import './temajs/app.js';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+import "./temajs/app.js";
--- resources/views/layouts/app/app.blade.php
+++ resources/views/layouts/app/app.blade.php
@@ -1,50 +1,49 @@
-
 <!DOCTYPE html>
 <html lang="tr">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="/img/icons/icon-48x48.png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="/img/icons/icon-48x48.png" />
 
-	<link rel="canonical" href="#" />
+    <link rel="canonical" href="#" />
 
-	<title>Admin Panel v1.1 </title>
+    <title>Admin Panel v1.1 </title>
 
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @vite(["resources/css/temacss/app.css"])
+    @vite(['resources/css/app.css'])
 
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-	<link href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css" rel="stylesheet">
 
 </head>
 
 <body>
-	<div class="wrapper">
-		@include('layouts.app.menu')
+    <div class="wrapper">
+        @include('layouts.app.menu')
 
-		<div class="main">
+        <div class="main">
             @include('layouts.app.navbar')
 
-			<main class="content">
-				<div class="container-fluid p-0">
+            <main class="content">
+                <div class="container-fluid p-0">
                     @yield('content')
-				</div>
-			</main>
+                </div>
+            </main>
 
-			@include('layouts.app.footer')
-		</div>
-	</div>
-
-
-	@vite(["resources/js/temajs/app.js"])
+            @include('layouts.app.footer')
+        </div>
+    </div>
 
 
-	@yield('js')
+    @vite(['resources/js/app.js'])
+
+
+    @yield('js')
 
 
 
